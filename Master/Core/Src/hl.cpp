9110b73d6c882b213979aa4d87ff34f355cec3bf
StarQTius <dussautoirp@gmail.com>
2022-01-08 19:18:42 +0100

    Fix typo & HL_Init

--- Master/Core/Src/hl.cpp
+++ Master/Core/Src/hl.cpp
@@ -11,6 +11,7 @@
 
 #define RX_BUF_SIZE 128
 
+static void HL_Write_Byte(uint8_t);
 static void HL_Interrupt(UART_HandleTypeDef *);
 static void HL_Interrupt_Get_Order(UART_HandleTypeDef *);
 static void HL_Interrupt_Call_Order(UART_HandleTypeDef *);
@@ -28,6 +29,19 @@ static uint8_t write_stuff_counter=0;
 static k2o::dispatcher dispatcher{rpc::master::keyring};
 static k2o::order *hanging_order = NULL;
 
+extern "C" void HL_Init(UART_HandleTypeDef *handler_ptr) {
+	huart_ptr = handler_ptr;
+	HL_Reset_Interrupt();
+}
+
+static uint32_t dummy_time_us = 0;
+extern "C" void HL_Send_Measure(void) {
+	auto report_measure_key = rpc::controller::keyring.get<controller_report_measure>();
+	HAL_UART_Transmit(huart_ptr, rpc::header, sizeof(rpc::header), timeout);
+	report_measure_key(dummy_time_us, 0, 0) >> HL_Write_Byte;
+	dummy_time_us += 1000000ull;
+}
+
 static void HL_Write_Byte(uint8_t byte) {
 	if (byte == 0xff){
 		write_stuff_counter++;
@@ -42,41 +56,25 @@ static void HL_Write_Byte(uint8_t byte) {
 	}
 }
 
-extern "C" void HL_Init(UART_HandleTypeDef *handler_ptr) {
-	huart_ptr = handler_ptr;
-
-}
-
-static uint32_t dummy_time_us = 0;
-extern "C" void HL_Send_Measure(void) {
-	auto report_measure_key = rpc::controller::keyring.get<controller_report_measure>();
-	HAL_UART_Transmit(huart_ptr, rpc::header, sizeof(rpc::header), timeout);
-	report_measure_key(dummy_time_us, 0, 0) >> HL_Write_Byte;
-	dummy_time_us += 1000000ull;
-}
-
-extern "C" void motion_set_translation_setpoint(uint32_t) {
-}
-
 //
 // Interrupt routines
 //
 
 void HL_Interrupt(UART_HandleTypeDef *) {
 	// Se charge de trouver une entête et de se placer au début d'une trame
-	// C'est la même logique que la fonction HL_Interrupt en mode bloquant, sauf qu'il faut traiter les octets dès qu'ils sont reçus.
+	// C'est la même logique que la fonction 'HL_Interrupt' en mode bloquant, sauf qu'il faut traiter les octets dès qu'ils sont reçus.
 	// Cette fonction doit être maintenu en tant qu'interruption tant qu'aucune entête complète n'a été trouvée.
 	// Lorsqu'une trame a été trouvée, la fonction 'HL_Interrupt_Get_Order' doit être appelé lorsque 2 octets ont été reçus pour réccupérer l'ordre à exécuter.
 	//
 	// ASTUCES :
-	//	- HAL_UART_Receive_IT(huart_ptr, rx_buf, 1) permet de rappeller la routine d'interruption lorsqu'un octet a été reçu et le place dans 'rx_buf'.
+	//	- HAL_UART_Receive_IT(huart_ptr, rx_buf, 1) permet de rappeler la routine d'interruption lorsqu'un octet a été reçu et le place dans 'rx_buf'.
 	//	- HAL_UART_RegisterCallback(huart_ptr, HAL_UART_RX_COMPLETE_CB_ID, [pointeur vers fonction]) permet de changer la fonction appellée lorsque le nombre d'octets demandés a été reçus.
 }
 
 void HL_Interrupt_Get_Order(UART_HandleTypeDef *) {
 	// Réccupère l'ordre à appeller en fonction de la valeur reçu
 	// Un pointeur vers l'ordre doit être maintenu dans 'hanging_order'.
-	// A la fin de cette fonction, la fonction 'HL_Interrupt_Call_Order' doit être appellé à chaque octet reçu jusqu'à ce que suffisament d'octets soient reçus pour exécuter l'ordre
+	// A la fin de cette fonction, la fonction 'HL_Interrupt_Call_Order' doit être appelé à chaque octet reçu jusqu'à ce que suffisament d'octets soient reçus pour exécuter l'ordre
 	//
 	// ASTUCES :
 	//	- La syntaxe pour réccupérer l'ordre est un peu particulière :
@@ -105,14 +103,21 @@ void HL_Interrupt_Call_Order(UART_HandleTypeDef *) {
 	//
 	// ASTUCES :
 	//	- Il faut traiter les octets un par un, en se chargeant du bourrage d'octet.
-	//	- La syntaxe suivante permet de réaliser l'ordre : (*hanging_order)(read_byte, write_byte)
+	//	- La syntaxe suivante permet de réaliser l'ordre : (*hanging_order)(read_byte, HL_Write_Byte)
 	//	  Après cette ligne, les octets à envoyer sont stockés dans 'tx_buf'.
-	//  - Ne pas oublier de renvoyer les octets dans 'tx_buf' (en mode bloquant) avec l'entête dédiée au réponse ! (la variable 'response_header')
-	//	- Ne pas oublier d'insérer l'octet indiquant le type de la trame (i.e. rpc::REQUEST).
+	//  - Ne pas oublier d'envoyer l'entête avant la trame.
+	//	- Ne pas oublier d'insérer l'octet indiquant le type de la trame (i.e. rpc::RESPONSE).
 	auto read_byte = [ptr = rx_buf]() mutable { return *ptr++; };
 }
 
 void HL_Reset_Interrupt(size_t) {
 	read_stuff_counter = 0;
 	HAL_UART_RegisterCallback(huart_ptr, HAL_UART_RX_COMPLETE_CB_ID, HL_Interrupt);
+}
+
+//
+// Order implementations
+//
+
+extern "C" void motion_set_translation_setpoint(uint32_t) {
 }
