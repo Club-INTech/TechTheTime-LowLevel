2fac90d4c5bc1d9681995f01ef3555f4fa4bedb7
StarQTius <dussautoirp@gmail.com>
2022-03-01 23:50:10 +0100

    Smoothen the motion control

--- Master/Core/Src/stm32g4xx_hal_msp.c
+++ Master/Core/Src/stm32g4xx_hal_msp.c
@@ -159,7 +159,7 @@ void HAL_TIM_Base_MspInit(TIM_HandleTypeDef* htim_base)
     /* Peripheral clock enable */
     __HAL_RCC_TIM3_CLK_ENABLE();
     /* TIM3 interrupt Init */
-    HAL_NVIC_SetPriority(TIM3_IRQn, 0, 0);
+    HAL_NVIC_SetPriority(TIM3_IRQn, 1, 0);
     HAL_NVIC_EnableIRQ(TIM3_IRQn);
   /* USER CODE BEGIN TIM3_MspInit 1 */
 
