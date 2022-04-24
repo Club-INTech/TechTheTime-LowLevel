1ae06826b9847873a8067c33bc93d6d0d0a3b150
StarQTius <dussautoirp@gmail.com>
2022-04-24 21:58:52 +0200

    master: Add Misc_* order for 2A

---
+++ Master/Core/Inc/misc.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "stm32g4xx_hal.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif // __cplusplus
+
+void Misc_Init(I2C_HandleTypeDef*);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif // __cplusplus
